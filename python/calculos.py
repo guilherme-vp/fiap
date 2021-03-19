b70fbd9f5f3be82659bce821325f74b8e0594679
Xubili <guivpw68@gmail.com>
2021-03-19 14:16:33 -0300

    feat: add calculos python

---
+++ python/calculos.py
@@ -0,0 +1,10 @@
+numero1 = int(input("Digite o valor do primeiro número: "))
+numero2 = int(input("Digite o valor do segundo número: "))
+numero3 = int(input("Digite o valor do terceiro número: "))
+numero4 = int(input("Digite o valor do quarto número: "))
+
+multiplicacao = numero1 * numero3
+soma = numero2 + numero4
+
+print("A multiplicação entre o primeiro e o terceiro valor é de:", multiplicacao)
+print("A soma entre o segundo e o quarto valor é de:", soma)
