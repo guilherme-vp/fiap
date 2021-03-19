c8f521ce2b0812cd372bdd528a056a0cd1cade4a
Xubili <guivpw68@gmail.com>
2021-03-19 14:16:12 -0300

    feat: add milionario python

---
+++ python/milionario.py
@@ -0,0 +1,9 @@
+salario = float(input("Digite o valor do seu salário: R$ "))
+despesas = float(input("Digite o valor das suas despesas mensais: R$ "))
+
+sobra = salario - despesas
+print("O valor total de sobra é de: R$", sobra)
+
+anos = (1000000/sobra)/12
+print("Para se tornar um milionário, você precisaria poupar",
+      round(anos, 2), 'anos de seu salário. FORÇA GUERREIRO(A)!')
