1897719cef73cede3183fb167fb000c26cc36a66
Xubili <guivpw68@gmail.com>
2021-03-19 14:15:32 -0300

    feat: add volume python

---
+++ python/volume.py
@@ -0,0 +1,7 @@
+raio = float(input("Digite o valor do raio da lata: "))
+
+altura = float(input("Digite a altura da lata: "))
+
+volume = 3.14 * raio ** 2 * altura
+
+print("O volume da lata de óleo é de:", volume, "m³")
