7ba4f9a62979c709aa901b6392765d84beffffed
Xubili <guivpw68@gmail.com>
2021-03-19 14:15:54 -0300

    feat: add quadrado 2 python

---
+++ python/quadrado2.py
@@ -0,0 +1,7 @@
+numero1 = int(input("Digite o valor do primeiro número: "))
+numero2 = int(input("Digite o valor do segundo número: "))
+numero3 = int(input("Digite o valor do terceiro número: "))
+
+quadrado = (numero1 + numero2 + numero3) ** 2
+
+print("A potência das somas é de:", quadrado)
