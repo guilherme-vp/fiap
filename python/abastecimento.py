0eb0a145cedeecd8920bb99f0665ca0b2a4e2634
Xubili <guivpw68@gmail.com>
2021-03-19 14:16:45 -0300

    feat: add abastecimento python

---
+++ python/abastecimento.py
@@ -0,0 +1,7 @@
+valorAbastecimento = int(input("Insira o valor de abastecimento: R$"))
+valorCombustivel = float(input("insira o preço do litro do combustível: R$ "))
+
+resultado = valorAbastecimento/valorAbastecimento
+
+print("Você comprará " + round(resultado) + "litros")
+
