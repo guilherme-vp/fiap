1ec5edecef67b520bd18efce9b44d66d4f9bd1e9
Xubili <guivpw68@gmail.com>
2021-03-19 14:15:42 -0300

    feat: add temperatura python

---
+++ python/temperatura.py
@@ -0,0 +1,6 @@
+temperaturaCelsius = input("Digite o valor da temperatura em graus Celsius: ")
+
+temperaturaFahrenheit = (int(temperaturaCelsius) * 9/5) + 32
+
+print("O valor da temperatura fornecida em graus Fahrenheit é de:",
+      temperaturaFahrenheit, "Fº")
