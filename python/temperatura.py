99db0afe1ce485b95b9c6d693add2d0b5b6783f5
Xubili <guivpw68@gmail.com>
2021-03-19 15:36:09 -0300

    refactor: add round to vars instead of return

--- python/temperatura.py
+++ python/temperatura.py
@@ -1,6 +1,7 @@
-temperaturaCelsius = input("Digite o valor da temperatura em graus Celsius: ")
+temperaturaCelsius = float(
+    input("Digite o valor da temperatura em graus Celsius: "))
 
-temperaturaFahrenheit = (int(temperaturaCelsius) * 9/5) + 32
+temperaturaFahrenheit = (temperaturaCelsius * 9/5) + 32
 
 print("O valor da temperatura fornecida em graus Fahrenheit é de:",
       temperaturaFahrenheit, "Fº")
