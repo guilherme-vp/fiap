985cc619d5acae47bb5e9dcdad6a646e84b14970
Xubili <guivpw68@gmail.com>
2021-03-19 14:16:23 -0300

    feat: add cotacao python

---
+++ python/cotacao.py
@@ -0,0 +1,7 @@
+cotacaoDolar = float(input("Digite o valor de cotação do dólar: "))
+valorReais = float(input("Digite a quantidade em reais: "))
+
+dolares = valorReais/cotacaoDolar
+
+print("O valor de", valorReais, "reais na cotação de",
+      cotacaoDolar, "é de:", dolares, "dólares.")
