8031e3b7e5c9ad95ee7db3a1bf54b3ba9dd54063
Guilherme Vieira <guivpw68@gmail.com>
2021-03-26 12:04:01 -0300

    feat: add checkpoint exercise

---
+++ checkpoint/checkpoint1.py
@@ -0,0 +1,31 @@
+import math as m
+
+nomeAtendente = input("Digite o nome do(a) recepcionista: ")
+qtdYoga = int(
+    input("Digite a quantidade de pessoas que frequentaram a atividade de yoga: "))
+qtdNatacao = int(input(
+    "Digite a quantidade de pessoas que frequentaram a atividade de natação: "))
+qtdHidro = int(input(
+    "Digite a quantidade de pessoas que frequentaram a atividade de hidroginástica: "))
+qtdGinastica = int(input(
+    "Digite a quantidade de pessoas que frequentaram a atividade de ginástica artística: "))
+qtdTotal = qtdYoga + qtdGinastica + qtdHidro + qtdNatacao
+
+
+def getPorcentagem(qtd):
+    porcentagemTotal = m.ceil(qtd/qtdTotal * 100)
+    return porcentagemTotal
+
+
+porcYoga = getPorcentagem(qtdYoga)
+porcNatacao = getPorcentagem(qtdNatacao)
+porcHidro = getPorcentagem(qtdHidro)
+porcGinastica = getPorcentagem(qtdGinastica)
+
+
+print('Prezado(a) ' + nomeAtendente + '.\nNa data de hoje,', qtdTotal,
+      'pessoas frequentaram o Clube de Campo Viver a vida com alegria.\n',
+      porcYoga, '% da atividade YOGA\n',
+      porcNatacao, '% da atividade NATAÇÃO\n',
+      porcHidro, '% da atividade HIDROGINÁSTICA\n',
+      porcGinastica, '% da atividade GINÁSTICA ARTÍSTICA')
