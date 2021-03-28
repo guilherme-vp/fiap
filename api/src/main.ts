a61e8aa6f4205084dcbe2e832ef29e27f9ceef3f
Guilherme Vieira <guivpw68@gmail.com>
2021-03-28 16:30:15 -0300

    feat: add express app

---
+++ api/src/main.ts
@@ -0,0 +1,15 @@
+import express from 'express'
+import cors from 'cors'
+import { routes } from 'routes/routes'
+
+const app = express()
+
+app.use(express.json)
+app.use(cors)
+app.use(routes)
+
+const port = process.env.PORT || 3000
+
+app.listen(port, () => {
+	console.debug(`Servidor rodando na porta ${port}`)
+})
