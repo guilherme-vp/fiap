f99e926ee672036dd8ae93135a50518d3999eb9b
Guilherme Vieira <guivpw68@gmail.com>
2021-03-28 23:45:45 -0300

    fix: ensure app use middlewares

--- api/src/main.ts
+++ api/src/main.ts
@@ -1,11 +1,12 @@
 import express from 'express'
 import cors from 'cors'
-import { routes } from 'routes/routes'
+import routes from 'routes/routes'
 
 const app = express()
 
-app.use(express.json)
-app.use(cors)
+app.use(express.json())
+app.use(cors())
+
 app.use(routes)
 
 const port = process.env.PORT || 3000
